90a4d8b0a537cbb6d4fb2b6e59f8e5e41def6e70
mistarson <yeowuli2@naver.com>
2022-09-30 01:04:50 +0900

    스프링 AOP 실전 예제 - 로그 출력 AOP - Annotation 만들기

---
+++ src/main/java/hello/aop/order/aop/exam/ExamService.java
@@ -0,0 +1,19 @@
+package hello.aop.order.aop.exam;
+
+
+import hello.aop.order.aop.exam.annotation.Trace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExamService {
+
+    private final ExamRepository examRepository;
+
+    @Trace
+    public void request(String itemId) {
+        examRepository.save(itemId);
+    }
+
+}
